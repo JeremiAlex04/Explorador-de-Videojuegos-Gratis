fb9dd586ca989abdc41c3e7c11e99f91c4ca60e4
Jeremi Alexander Olivares Chavez <jeremialex04@gmail.com>
2025-11-20 22:37:46 -0500

    Update API proxy configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/games": {
-        target: "https://www.freetogame.com/api/games",
+      '/api': {
+        target: 'https://www.freetogame.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/games/, "")
       },
-      "/api/game": {
-        target: "https://www.freetogame.com/api/game",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/game/, "")
-      }
-    }
-  }
-});
+    },
+  },
+})
