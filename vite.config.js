ff095ceb185889a377d3455804887f903166ee53
Jeremi Alexander Olivares Chavez <jeremialex04@gmail.com>
2025-11-20 22:28:28 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,10 +6,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://www.freetogame.com',
+      '/api/games': {
+        target: 'https://www.freetogame.com/api/games',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/games/, '')
       },
-    },
-  },
+      '/api/game': {
+        target: 'https://www.freetogame.com/api/game',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/game/, '')
+      }
+    }
+  }
 })
